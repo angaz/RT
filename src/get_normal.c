c88ffc49fe8a804b1293135dbc1830a097f996aa
Robert JONES <rojones@e5r10p5.jnb.42.fr>
2016-08-19 09:35:41 +0200

    fix save prim conver from radians back to degrees normal of plain is by directional init_of vairiables in read scene fix preventing malloc error on empty RT_SCENE files

--- src/get_normal.c
+++ src/get_normal.c
@@ -6,7 +6,7 @@
 /*   By: rojones <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/08 12:10:39 by rojones           #+#    #+#             */
-/*   Updated: 2016/08/14 14:56:07 by adippena         ###   ########.fr       */
+/*   Updated: 2016/08/19 09:33:16 by rojones          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,18 @@
 static t_vector	get_con_normal(t_env *e, t_vector ray)
 {
 	t_vector    normal;
-	t_vector	pro;
 	t_vector    rot;
 	t_vector	p_par;
 	t_vector	p_orth;
 	t_vector	nnor_orth;
-	double		angle;
 
-	pro = vproject(vsub(ray, e->p_hit->loc), e->p_hit->dir);
-	normal = vunit(vsub(vsub(ray, e->p_hit->loc), pro));
+	normal = vunit(vsub(vsub(ray, e->p_hit->loc),
+				vproject(vsub(ray, e->p_hit->loc), e->p_hit->dir)));
 	rot = vunit(vcross(normal, e->p_hit->dir));
 	p_par = vproject(normal, rot);
 	p_orth = vsub(normal, p_par);
-	angle = e->p_hit->angle;
-	nnor_orth = vadd(vmult(p_orth, cos(angle)), vmult(vcross(rot, p_orth), sin(angle)));
+	nnor_orth = vadd(vmult(p_orth, cos(e->p_hit->angle)),
+			vmult(vcross(rot, p_orth), sin(e->p_hit->angle)));
 	normal = vunit(vadd(nnor_orth, p_par));
 	return (normal);
 }
@@ -40,7 +38,9 @@ t_vector	get_normal(t_env *e, t_vector ray)
 	if (e->p_hit->type == PRIM_SPHERE)
 		return (vunit(vdiv(vsub(ray, e->p_hit->loc), e->p_hit->radius)));
 	if (e->p_hit->type == PRIM_PLANE)
-		return (vunit(e->p_hit->normal));
+		return ((vdot(e->p_hit->normal, ray) < -0.44807361612) ? 
+				vunit(e->p_hit->normal) : 
+				vunit(vsub((t_vector){0, 0, 0}, e->p_hit->normal)));
 	if (e->p_hit->type == PRIM_CYLINDER)
 		return (vunit(vsub(vsub(ray, e->p_hit->loc),
 			vproject(vsub(ray, e->p_hit->loc), e->p_hit->dir))));
